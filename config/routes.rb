6756e342b4f8ed6eb916bfc1010f53233ed41b44
Rishad Alam <rishad.s.a@gmail.com>
2023-07-26 11:20:32 -0400

    rails generate controller about for about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'about/index'
   root to: 'products#index'
 
+  resources :products, only: [:index, :show]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -11,7 +13,7 @@ Rails.application.routes.draw do
   end
 
   resources :orders, only: [:create, :show]
-
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
