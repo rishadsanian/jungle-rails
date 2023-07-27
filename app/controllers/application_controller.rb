d5d83f9a161dda39401986846fe5086c3093f9f2
Rishad Alam <rishad.s.a@gmail.com>
2023-07-26 22:22:48 -0400

    ad user_params to user contoller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,20 @@ class ApplicationController < ActionController::Base
     }
     cookies[:cart]
   end
-
   
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  #assign current user based on session cookies
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  #direct to login page unless user is logged in already
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
 end
