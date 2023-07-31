2e76bd16f9b94a189d24dda81e089ee6d2fbcaa7
Rishad Alam <rishad.s.a@gmail.com>
2023-07-30 21:46:08 -0400

    add cypress testing to ensure successful naviagtion to product details page from home page and checking for the right product name on both pages

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+
+describe("Jungle Product Details page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("Users can navigate from the home page to the correct product detail page", () => {
+    let productName;
+    cy.get(".products article")
+      .first("article")
+      .then(($article) => {
+        productName = $article.find("h1 span").text().split("$")[0];
+        console.log(productName);
+        cy.get(".products article")
+          .first("article")
+          .click()
+          .then(() => {
+            console.log(productName);
+            cy.get(".product-detail").should("be.visible");
+            cy.get(".product-detail").should("contain", productName);
+          });
+      });
+  });
+});
